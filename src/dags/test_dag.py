10870537be53769eaa4f563cb9d5953df261d525
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-15 21:02:09 +0300

    Init commit

---
+++ src/dags/test_dag.py
@@ -0,0 +1,15 @@
+import datetime
+from airflow.models.dag import DAG
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id='my_minimal_dag',
+    start_date=datetime.datetime(2023, 1, 1),
+    schedule='@daily',
+    catchup=False,  # Set to False to avoid backfilling past runs
+    tags=['example'],
+) as dag:
+    start_task = EmptyOperator(task_id='start_task')
+    end_task = EmptyOperator(task_id='end_task')
+
+    start_task >> end_task
