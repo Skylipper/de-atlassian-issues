79f09786ffe749e35b0bc21b830e6a4e67ec60e4
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-16 14:45:55 +0300

    Init commit

--- src/dags/stg_init_tables_dag.py
+++ src/dags/stg_init_tables_dag.py
@@ -15,15 +15,15 @@ log = logging.getLogger("ds_init_dag")
     schedule_interval=None,
     start_date=pendulum.datetime(2022, 5, 5, tz="UTC"),
     catchup=False,
-    tags=['sprint5', 'project', 'init', 'manual'],
-    template_searchpath=['src'],
+    tags=['project', 'init', 'sql'],
+    template_searchpath=['../sql'],
     is_paused_upon_creation=True
 )
 def init_stg_dag():
     init_stg_issues = SQLExecuteQueryOperator(
         task_id="init_stg_issues",
         conn_id=var.DWH_CONNECTION_NAME,
-        sql="sql/stg_init_issues.sql.sql",
+        sql="stg_init_issues.sql.sql",
         autocommit=True
     )
 
