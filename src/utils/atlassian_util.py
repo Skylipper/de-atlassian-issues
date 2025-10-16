40592e7f8a64dadf3052a48a49e795894b544287
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-16 16:13:49 +0300

    Init commit

--- src/utils/atlassian_util.py
+++ src/utils/atlassian_util.py
@@ -19,14 +19,46 @@ def get_jql_query(date = var.START_DATE):
 def get_jql_results(jql_query = None):
     conn_info = get_atl_connection_info()
     url = f"{conn_info.host}/{var.API_SEARCH_METHOD_PATH}?jql=issue%20%3D%20JRASERVER-3943"
-    logging.info(f"url: {url}")
 
     payload = {}
+    headers = get_atl_headers(conn_info)
+
+    response = execute_request("GET", url, headers, payload, 200)
+
+    return response
+
+def get_atl_headers(conn_info):
     headers = {
         'Accept': 'application/json',
         'Authorization': f'Bearer {conn_info.password}'
     }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    return headers
 
-    print(response.text)
+def execute_request(method, url, headers, payload, expected_code):
+    logging.info(f"method: {method}, url: {url}")
+
+    response_code = 0
+    response = None
+    retries = 0
+
+    for i in range (0, var.REQUEST_RETRY_LIMIT):
+        response = requests.request("GET", url, headers=headers, data=payload)
+        response_code = response.status_code
+        if response_code == expected_code:
+            break
+        retries += 1
+
+    if response_code != expected_code:
+        raise Exception(f"Request failed with code {response_code}, expected {expected_code}")
+
+    # while response_code != expected_code:
+    #     response = requests.request("GET", url, headers=headers, data=payload)
+    #     response_code = response.status_code
+    #     retries += 1
+    #
+    #     if retries > var.REQUEST_RETRY_LIMIT:
+    #         raise
+
+
+    return response.json()
