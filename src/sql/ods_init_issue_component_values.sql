d30a57a260de9e124136c4f5e4b6ddfc254d6f3b
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-22 19:51:36 +0300

    +

--- src/sql/ods_init_issue_component_values.sql
+++ src/sql/ods_init_issue_component_values.sql
@@ -1,12 +1,13 @@
 create table if not exists ods.issue_component_values
 (
     id             serial primary key not null,
-    issue_id       int         not null,
+    issue_id       int                not null,
     project_id     int,
     component_id   int,
-    component_name varchar(256)
+    component_name varchar(256),
+    updated_ts     timestamp
 );
 
 alter table ods.issue_component_values
     add constraint issue_component_id_unique
-        unique (issue_id,component_id);
+        unique (issue_id, component_id);
--- src/sql/ods_load_issue_component_values.sql
+++ src/sql/ods_load_issue_component_values.sql
@@ -3,15 +3,17 @@ WITH last_updated as (SELECT COALESCE(
                                       FROM ods.load_settings
                                       WHERE workflow_key = 'ods.issue_component_values'),
                                      '2010-01-01'::timestamp) as last_loaded_ts)
+   , max_loaded as (SELECT MAX(update_ts) last_updated FROM stg.issues)
 
 INSERT
-INTO ods.issue_component_values (issue_id, project_id, component_id, component_name)
+INTO ods.issue_component_values (issue_id, project_id, component_id, component_name, update_ts)
 SELECT object_id::int                                               as issue_id,
        (object_value::jsonb -> 'fields' -> 'project' ->> 'id')::int as project_id,
        (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'components')::jsonb ->>
         'id')::int                                                  as component_id,
        (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'components')::jsonb ->>
-        'name')::varchar(255)                                       as component_name
+        'name')::varchar(255)                                       as component_name,
+       (SELECT last_updated from max_loaded)                        as update_ts
 FROM stg.issues
 WHERE update_ts >= (SELECT last_loaded_ts FROM last_updated)
 ON CONFLICT (issue_id,component_id) DO UPDATE
