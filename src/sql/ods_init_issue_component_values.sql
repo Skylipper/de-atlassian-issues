58206d9d807b38b44d6ae42dfd45f3682418bae0
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-22 19:33:26 +0300

    +

--- src/sql/ods_init_issue_component_values.sql
+++ src/sql/ods_init_issue_component_values.sql
@@ -1,8 +1,8 @@
 create table if not exists ods.issue_component_values
 (
     id             serial primary key not null,
-    issue_id       numeric(8)         not null,
-    project_id     numeric(8),
-    component_id   numeric(8),
+    issue_id       int         not null,
+    project_id     int,
+    component_id   int,
     component_name varchar(256)
 );
--- src/sql/ods_load_issue_component_values.sql
+++ src/sql/ods_load_issue_component_values.sql
@@ -6,11 +6,11 @@ WITH last_updated as (SELECT COALESCE(
 
 INSERT
 INTO ods.issue_component_values (issue_id, project_id, component_id, component_name)
-SELECT object_id                                                                                                    as issue_id,
-       (object_value::jsonb -> 'fields' -> 'project' ->> 'id')::numeric(10)                                         as project_id,
+SELECT object_id::int                                                                                                as issue_id,
+       (object_value::jsonb -> 'fields' -> 'project' ->> 'id')::int                                         as project_id,
        (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'components')::jsonb ->>
-        'id')::numeric(10)                                                                                          as component_id,
+       'id')::int                                                                                          as component_id,
        (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'components')::jsonb ->>
-        'name')::varchar(255)                                                                                       as component_name
+       'name')::varchar(255)                                                                                       as component_name
 FROM stg.issues
 WHERE update_ts >= (SELECT last_loaded_ts FROM last_updated);
