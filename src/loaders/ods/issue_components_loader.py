71674d8808ecd47862896f6270c0d5ab6b31583f
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-23 10:34:08 +0300

    +

--- src/loaders/ods/issue_components_loader.py
+++ src/loaders/ods/issue_components_loader.py
@@ -6,7 +6,7 @@ def load_issue_components():
     last_updated = dwh_util.get_last_loaded_ts(var.ODS_WF_TABLE_NAME, var.ODS_ISSUE_COMPS_TABLE_NAME)
     with conn:
         cur = conn.cursor()
-        query = dwh_util.get_query_string_from_file(f'{var.AIRFLOW_DAGS_DIR}/src/sql/ods/ods_load_issue_component_values.sql')
+        query = dwh_util.get_query_string_from_file(f'{var.AIRFLOW_DAGS_DIR}/src/sql/ods/load_issue_component_values.sql')
         cur.execute(query)
         cur.execute(f"SELECT COALESCE(MAX(update_ts),'{last_updated}') last_updated FROM stg.issues;")
         update_ts = cur.fetchall()[0][0]
