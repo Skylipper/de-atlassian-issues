476dca4cedb204842ce41b08d5cf22b74074e31b
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-23 17:53:19 +0300

    +

--- src/sql/ods/load_issue_fix_version_values.sql
+++ src/sql/ods/load_issue_fix_version_values.sql
@@ -23,9 +23,9 @@ INSERT
 INTO ods.issue_fix_version_values (issue_id, project_id, version_id, version_name, update_ts)
 SELECT object_id::int                                               as issue_id,
        (object_value::jsonb -> 'fields' -> 'project' ->> 'id')::int as project_id,
-       (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'versions')::jsonb ->>
+       (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'fixVersions')::jsonb ->>
         'id')::int                                                  as version_id,
-       (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'versions')::jsonb ->>
+       (jsonb_array_elements_text(object_value::jsonb -> 'fields' -> 'fixVersions')::jsonb ->>
         'name')::varchar(255)                                       as version_name,
        update_ts                                                    as update_ts
 FROM stg.issues
