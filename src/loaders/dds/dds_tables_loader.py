34b3b9cd61be8032ec866fb3cbf13fad568bd95d
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-28 21:01:57 +0300

    +

--- src/loaders/dds/dds_tables_loader.py
+++ src/loaders/dds/dds_tables_loader.py
@@ -6,7 +6,7 @@ def load_data_for_table(schema, table, stat_table, update_field):
     sql_file_path = f'{var.AIRFLOW_DAGS_DIR}/src/sql/{schema}/load_{table}.sql'
 
     conn = dwh_util.get_dwh_connection()
-    last_updated = dwh_util.get_last_loaded_ts(stat_table, table)
+    last_updated = dwh_util.get_last_loaded_ts(stat_table, f'{schema}.{table}')
     with conn:
         cur = conn.cursor()
         # Insert and update data
