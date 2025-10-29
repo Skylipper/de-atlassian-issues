37d81e50fa4e0e1617ec30b7ccfb58f3ac533ba0
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-29 17:30:47 +0300

    +

--- src/utils/http_requests_util.py
+++ src/utils/http_requests_util.py
@@ -15,7 +15,8 @@ def execute_request(method, url, headers, payload):
     retries = 0
 
     for i in range(0, var.REQUEST_RETRY_LIMIT):
-        response = requests.request("GET", url, headers=headers, data=payload)
+        response = requests.request("GET", url, headers=headers, data=payload, timeout=15)
+
         response_code = response.status_code
         if response.ok:
             break
