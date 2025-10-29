cc08754e22d434bbd39c27c69fc09ec560f2e659
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-29 16:33:38 +0300

    +

--- src/dags/dds_load_dag.py
+++ src/dags/dds_load_dag.py
@@ -9,18 +9,6 @@ import src.loaders.dds.dds_tables_loader as dtl
 log = logging.getLogger("load_objects")
 
 
-# def load_d_projects_f():
-#     dtl.load_d_projects()
-#
-#
-# def load_d_priorities_f():
-#     dtl.load_d_priorities()
-#
-#
-# def load_d_components_f():
-#     dtl.load_d_components()
-
-
 @dag(
     start_date=datetime(2025, 10, 22),
     schedule='*/10 * * * *',
@@ -29,27 +17,48 @@ log = logging.getLogger("load_objects")
     tags=['load', 'project', 'dds', 'atlassian'],
 )
 def load_dds_tables():
-    load_d_projects = PythonOperator(
-        task_id='load_d_projects',
+    load_dds_d_projects = PythonOperator(
+        task_id='load_dds_d_projects',
         python_callable=dtl.load_d_projects
     )
 
-    load_d_priorities = PythonOperator(
-        task_id='load_d_priorities',
+    load_dds_d_priorities = PythonOperator(
+        task_id='load_dds_d_priorities',
         python_callable=dtl.load_d_priorities
     )
 
-    load_dds_components = PythonOperator(
-        task_id='load_dds_components',
+    load_dds_d_components = PythonOperator(
+        task_id='load_dds_d_components',
         python_callable=dtl.load_d_components
     )
 
-    load_dds_issuetypes = PythonOperator(
-        task_id='load_dds_issuetypes',
+    load_dds_d_issuetypes = PythonOperator(
+        task_id='load_dds_d_issuetypes',
         python_callable=dtl.load_d_issuetypes
     )
 
-    [load_d_projects, load_d_priorities, load_dds_issuetypes] >> load_dds_components
+    load_dds_d_resolutions = PythonOperator(
+        task_id='load_dds_d_resolutions',
+        python_callable=dtl.load_d_resolutions
+    )
+
+    load_dds_d_statuses = PythonOperator(
+        task_id='load_dds_d_statuses',
+        python_callable=dtl.load_d_statuses
+    )
+
+    load_dds_d_users = PythonOperator(
+        task_id='load_dds_d_users',
+        python_callable=dtl.load_d_users
+    )
+
+    load_dds_d_versions = PythonOperator(
+        task_id='load_dds_d_versions',
+        python_callable=dtl.load_d_versions
+    )
+
+    [load_dds_d_projects, load_dds_d_priorities, load_dds_d_issuetypes,
+     load_dds_d_resolutions, load_dds_d_statuses, load_dds_d_users] >> [load_dds_d_components, load_dds_d_versions]
 
 
 dag = load_dds_tables()
