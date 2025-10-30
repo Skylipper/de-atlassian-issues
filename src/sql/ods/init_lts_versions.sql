174ea113bfe577dd9925895975fd4b2c945c3d60
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-30 16:46:17 +0300

    +

--- src/sql/ods/init_lts_versions.sql
+++ src/sql/ods/init_lts_versions.sql
@@ -3,5 +3,6 @@ create table ods.lts_versions
     id           serial primary key,
     version_name varchar(255) not null
         constraint lts_versions_version_name_unique
-            unique
+            unique,
+    update_ts    timestamp    not null
 );
--- src/sql/ods/load_lts_versions.sql
+++ src/sql/ods/load_lts_versions.sql
@@ -1,5 +1,5 @@
-INSERT INTO ods.lts_versions (version_name)
-SELECT DISTINCT object_id as version_name
+INSERT INTO ods.lts_versions (version_name, update_ts)
+SELECT DISTINCT object_id as version_name, now() as update_ts
 FROM stg.lts_versions
 ORDER BY object_id
 ON CONFLICT (version_name) DO NOTHING
