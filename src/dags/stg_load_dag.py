1fa59593dbf545f5bebc84eaea16fc75c56915a8
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-10-30 16:00:43 +0300

    +

--- src/dags/stg_load_dag.py
+++ src/dags/stg_load_dag.py
@@ -24,7 +24,7 @@ def load_stg_raw_data():
     load_issues_task = PythonOperator(
         task_id='loads_issues',
         python_callable=load_issues,
-        log=log
+        op_kwargs={'log': log }
     )
     load_lts_versions_task = PythonOperator(
         task_id='load_stg_lts_versions',
