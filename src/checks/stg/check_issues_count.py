b4452ffc251a5904e56b5b9c7ed1cacc931ec4db
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-11-01 16:03:55 +0300

    +

--- src/checks/stg/check_issues_count.py
+++ src/checks/stg/check_issues_count.py
@@ -6,7 +6,9 @@ from src.utils import dwh_util, atlassian_util
 
 
 def get_jql_results_count():
-    last_updated_date = dwh_util.get_last_loaded_ts(var.STG_WF_TABLE_NAME, var.STG_ISSUES_TABLE_NAME)
-    jql_query = f"""project in ('{var.SRV_PROJECT_KEY}','{var.CLOUD_PROJECT_KEY}') AND 'updated' >= '{var.START_DATE.strftime("%Y-%m-%d %H:%M")} and updated <= '{last_updated_date.strftime("%Y-%m-%d %H:%M")}'"""
+    last_updated_date = dwh_util.get_last_loaded_ts(var.STG_WF_TABLE_NAME, var.STG_ISSUES_TABLE_NAME).strftime("%Y-%m-%d %H:%M")
+    jql_query = f"""project in ('{var.SRV_PROJECT_KEY}','{var.CLOUD_PROJECT_KEY}') AND 'updated' >= '{var.START_DATE.strftime("%Y-%m-%d %H:%M")} and 'updated' <= '{last_updated_date}'"""
+    print(jql_query)
 
     count = atlassian_util.get_jql_results_count(jql_query)
+    return count
