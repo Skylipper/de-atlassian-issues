a57d5e1f01b7840bcda5e1651f48e1b0a5697780
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-11-18 12:07:57 +0300

    bugfix

--- src/sql/dds/load_d_issuetypes.sql
+++ src/sql/dds/load_d_issuetypes.sql
@@ -4,16 +4,25 @@ WITH last_updated as (SELECT COALESCE(
                                       WHERE workflow_key = 'dds.d_issuetypes'),
                                      '2010-01-01'::timestamp) as last_loaded_ts)
 
-INSERT INTO dds.d_issuetypes (issuetype_id, issuetype_name, update_ts)
+   , issuetypes as (SELECT DISTINCT issuetype_id, issuetype_name, update_ts
+                    FROM (SELECT issuetype_id,
+                                 issuetype_name,
+                                 updated                                                             as update_ts,
+                                 ROW_NUMBER() OVER (PARTITION BY issuetype_id ORDER BY updated DESC) as rn
+                          FROM ods.issues issues
+                          WHERE issues.updated >= (SELECT last_loaded_ts FROM last_updated)) a
+                    WHERE rn = 1)
+
+INSERT
+INTO dds.d_issuetypes (issuetype_id, issuetype_name, update_ts)
 SELECT DISTINCT issuetype_id,
                 issuetype_name,
-                now()
-FROM ods.issues issues
-WHERE issues.updated >= (SELECT last_loaded_ts FROM last_updated)
+                update_ts
+FROM issuetypes
 ORDER BY issuetype_id
 ON CONFLICT (issuetype_id) DO UPDATE
     SET issuetype_name = EXCLUDED.issuetype_name,
-        update_ts = now()
+        update_ts      = now()
 WHERE d_issuetypes.issuetype_name IS DISTINCT FROM EXCLUDED.issuetype_name
 
 
--- src/sql/dds/load_d_priorities.sql
+++ src/sql/dds/load_d_priorities.sql
@@ -3,17 +3,24 @@ WITH last_updated as (SELECT COALESCE(
                                       FROM dds.load_settings
                                       WHERE workflow_key = 'dds.d_priorities'),
                                      '2010-01-01'::timestamp) as last_loaded_ts)
+   , priorities as (SELECT priority_id, priority_name, update_ts
+                    FROM (SELECT DISTINCT priority_id,
+                                          priority_name,
+                                          updated                                                            as update_ts,
+                                          ROW_NUMBER() OVER (PARTITION BY priority_id ORDER BY updated DESC) as rn
+                          FROM ods.issues issues
+                          WHERE issues.priority_id is not null
+                            AND issues.updated >= (SELECT last_loaded_ts FROM last_updated)) a
+                    WHERE rn = 1)
 
-INSERT INTO dds.d_priorities (priority_id, priority_name, update_ts)
+INSERT
+INTO dds.d_priorities (priority_id, priority_name, update_ts)
 SELECT DISTINCT priority_id,
                 priority_name,
-                now()
-FROM ods.issues issues
-WHERE issues.priority_id is not null
-  AND issues.updated >= (SELECT last_loaded_ts FROM last_updated)
-ORDER BY priority_id
+                update_ts
+FROM priorities
 ON CONFLICT (priority_id) DO UPDATE
     SET priority_name = EXCLUDED.priority_name,
-        update_ts = now()
+        update_ts     = EXCLUDED.update_ts
 WHERE d_priorities.priority_name IS DISTINCT FROM EXCLUDED.priority_name
 
