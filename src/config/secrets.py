31ad11aca9bd646d72843893a21988a2a1561e57
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-11-21 10:56:21 +0300

    spark

---
+++ src/config/secrets.py
@@ -0,0 +1,16 @@
+# Clickhouse connection
+click_host = ""
+click_port = 8443
+click_db_name = ""
+click_db_table = ""
+click_user = ""
+click_password = ""
+
+
+# DWH connection
+dwh_host = ""
+dwh_port = 5432
+dwh_db_name = "dwh"
+dwh_user = ""
+dwh_password = ""
+
