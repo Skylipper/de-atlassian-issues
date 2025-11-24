9275f384407ec051f88036fe2464dc88934031b3
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-11-24 13:33:05 +0300

    spark

--- src/sql/ods/check_updated_issue_comp_count.sql
+++ src/sql/ods/check_updated_issue_comp_count.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(*)
+SELECT COUNT(DISTINCT issue_id)
 FROM ods.issue_component_values
 WHERE update_ts >= CURRENT_DATE
--- src/sql/ods/check_updated_issue_fix_ver_count.sql
+++ src/sql/ods/check_updated_issue_fix_ver_count.sql
@@ -1,3 +1,3 @@
-SELECT COUNT(*)
+SELECT COUNT(DISTINCT issue_id)
 FROM ods.issue_fix_version_values
 WHERE update_ts >= CURRENT_DATE
