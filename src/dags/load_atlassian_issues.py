453f8543a23dcf58c4162208f5aba4d86ab8828b
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-11-24 16:34:50 +0300

    spark

--- src/dags/load_atlassian_issues.py
+++ src/dags/load_atlassian_issues.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 
 from airflow.decorators import dag
+from airflow.operators.dummy_operator import DummyOperator
 from airflow.operators.python import PythonOperator
 from airflow.operators.sql import SQLValueCheckOperator, SQLCheckOperator
 from airflow.utils.task_group import TaskGroup
@@ -25,12 +26,14 @@ log = logging.getLogger("load_issues")
 )
 def load_atlassian_data():
     with TaskGroup("stg_issues_loader") as stg_issues_loader:
-        load_issues_task = PythonOperator(
+        stg_begin = DummyOperator(task_id="stg_begin")
+
+        stg_load_issues_task = PythonOperator(
             task_id='loads_stg_issues',
             python_callable=load_issues,
             op_kwargs={'log': log}
         )
-        load_lts_versions_task = PythonOperator(
+        stg_load_lts_versions_task = PythonOperator(
             task_id='load_stg_lts_versions',
             python_callable=load_lts_versions,
             op_kwargs={'log': log}
@@ -44,62 +47,65 @@ def load_atlassian_data():
                                                       tolerance=0.01,
                                                       on_failure_callback=check_util.inform_somebody)
 
-        load_issues_task >> stg_issue_count_check >> load_lts_versions_task
+        stg_begin >> stg_load_issues_task >> stg_issue_count_check >> stg_load_lts_versions_task
 
     with TaskGroup("ods_issues_loader") as ods_issues_loader:
-        load_issue_components_task = PythonOperator(
+        ods_begin = DummyOperator(task_id="ods_begin")
+
+        ods_load_issue_components_task = PythonOperator(
             task_id='load_issue_components',
             python_callable=otl.load_issue_components
         )
 
-        check_issue_comp_count = SQLValueCheckOperator(task_id="check_issue_comp_count_jql_sql",
-                                                       conn_id=var.DWH_CONNECTION_NAME,
-                                                       sql="ods/check_updated_issue_comp_count.sql",
-                                                       pass_value=check_util.get_today_issue_components_count(),
-                                                       tolerance=0.01,
-                                                       on_failure_callback=check_util.inform_somebody)
+        ods_check_issue_comp_count = SQLValueCheckOperator(task_id="check_issue_comp_count_jql_sql",
+                                                           conn_id=var.DWH_CONNECTION_NAME,
+                                                           sql="ods/check_updated_issue_comp_count.sql",
+                                                           pass_value=check_util.get_today_issue_components_count(),
+                                                           tolerance=0.01,
+                                                           on_failure_callback=check_util.inform_somebody)
 
-        load_issue_versions_task = PythonOperator(
+        ods_load_issue_versions_task = PythonOperator(
             task_id='load_issue_versions',
             python_callable=otl.load_issue_versions
         )
 
-        check_issue_version_count = SQLValueCheckOperator(task_id="check_issue_version_count_jql_sql",
-                                                          conn_id=var.DWH_CONNECTION_NAME,
-                                                          sql="ods/check_updated_issue_version_count.sql",
-                                                          pass_value=check_util.get_today_issue_version_count(),
-                                                          tolerance=0.01,
-                                                          on_failure_callback=check_util.inform_somebody)
+        ods_check_issue_version_count = SQLValueCheckOperator(task_id="check_issue_version_count_jql_sql",
+                                                              conn_id=var.DWH_CONNECTION_NAME,
+                                                              sql="ods/check_updated_issue_version_count.sql",
+                                                              pass_value=check_util.get_today_issue_version_count(),
+                                                              tolerance=0.01,
+                                                              on_failure_callback=check_util.inform_somebody)
 
-        load_issue_fix_versions_task = PythonOperator(
+        ods_load_issue_fix_versions_task = PythonOperator(
             task_id='load_issue_fix_versions',
             python_callable=otl.load_issue_fix_versions
         )
 
-        check_issue_fix_ver_count = SQLValueCheckOperator(task_id="check_issue_fix_ver_count_jql_sql",
+        ods_check_issue_fix_ver_count = SQLValueCheckOperator(task_id="check_issue_fix_ver_count_jql_sql",
                                                           conn_id=var.DWH_CONNECTION_NAME,
                                                           sql="ods/check_updated_issue_fix_ver_count.sql",
                                                           pass_value=check_util.get_today_issue_fix_ver_count(),
                                                           tolerance=0.01,
                                                           on_failure_callback=check_util.inform_somebody)
 
-        load_issues_task = PythonOperator(
+        ods_load_issues_task = PythonOperator(
             task_id='load_issues',
             python_callable=otl.load_issues
         )
-        check_issues_count = SQLCheckOperator(task_id="check_issues_count",
+        ods_check_issues_count = SQLCheckOperator(task_id="check_issues_count",
                                               conn_id=var.DWH_CONNECTION_NAME,
                                               sql="ods/check_issues_count.sql",
                                               on_failure_callback=check_util.inform_somebody)
 
-        load_lts_versions_task = PythonOperator(
+        ods_load_lts_versions_task = PythonOperator(
             task_id='load_lts_versions',
             python_callable=otl.load_lts_versions
         )
 
-        [load_issue_components_task >> check_issue_comp_count, load_issue_versions_task >> check_issue_version_count,
-         load_issue_fix_versions_task >> check_issue_fix_ver_count,
-         load_lts_versions_task] >> load_issues_task >> check_issues_count
+        ods_begin >> [ods_load_issue_components_task >> ods_check_issue_comp_count,
+                      ods_load_issue_versions_task >> ods_check_issue_version_count,
+                      ods_load_issue_fix_versions_task >> ods_check_issue_fix_ver_count,
+                      ods_load_lts_versions_task] >> ods_load_issues_task >> ods_check_issues_count
 
     stg_issues_loader >> ods_issues_loader
 
