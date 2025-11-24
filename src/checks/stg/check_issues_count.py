34b0059389da8c8f80bea43fb08083f1b305e458
Andrei Lipovchenko <alipovchenko@yandex.ru>
2025-11-24 11:16:29 +0300

    spark

--- src/checks/stg/check_issues_count.py
+++ src/checks/stg/check_issues_count.py
@@ -1,14 +1,14 @@
 import src.config.variables as var
 from src.utils import dwh_util, atlassian_util
 
-# project in ('JRASERVER','JRACLOUD') AND 'updated' >= '2010-01-01 00:00'
-
-
+# Проверим, что количество обновленных запросов с начала дня одинаково в JQL и в БД
 
 def get_jql_results_count():
-    last_updated_date = dwh_util.get_last_loaded_ts(var.STG_WF_TABLE_NAME, var.STG_ISSUES_TABLE_NAME).strftime("%Y-%m-%d %H:%M")
-    jql_query = f"""project in ('{var.SRV_PROJECT_KEY}','{var.CLOUD_PROJECT_KEY}') AND 'updated' >= '{var.START_DATE.strftime("%Y-%m-%d %H:%M")}' and 'updated' <= '{last_updated_date}'"""
+    jql_query = f"""({var.PLAIN_JQL}) AND 'updated' >= startOfDay()"""
     print(jql_query)
 
     count = atlassian_util.get_jql_results_count(jql_query)
+    print(count)
+    print(type(count))
+
     return count
